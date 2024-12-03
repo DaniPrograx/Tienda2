6cbbddc038ec23ac0c7df44a3d558122e2b57e60
PabloRodCe <jorodriguezce@est.utn.ac.cr>
2024-12-03 00:22:23 -0600

    se agrega el mapper las img y paquete utils

---
+++ src/Model/Mapper/Mapper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Model.Mapper;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sibaj
+ */
+public interface Mapper <Ent, DTO>{
+    public DTO toDTO(Ent ent);
+    public Ent toEnt(DTO dto)throws SQLException;
+}
